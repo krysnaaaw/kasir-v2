427e5a7a3aea499a6e84963051823a548d5090f6
krysnaaw <kresnawicaksono793@gmail.com>
2026-06-20 18:53:18 +0700

    api fiks

--- src/pages/auth/RegisterPage.jsx
+++ src/pages/auth/RegisterPage.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import toast from 'react-hot-toast';
-import api from '../../services/api';
+import api from '../services/api';
 
 const RegisterPage = () => {
   const navigate = useNavigate();
